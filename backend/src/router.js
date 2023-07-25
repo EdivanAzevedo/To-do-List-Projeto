1237091873f89d0558ecd72ff289ec5f6191ca7f
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-07-24 21:52:51 -0300

    Finalização da API

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const tasksController = require('./controllers/taskController')
+const express = require("express");
 const router = express.Router();
 
-router.get('/tasks', tasksController.getAll)
+const tasksController = require("./controllers/taskController");
+const taskMiddlewares = require("./middlewares/tasksMiddlewares");
+
+router.get("/tasks", tasksController.getAll);
+router.post("/tasks", taskMiddlewares.validateFielTitle, tasksController.createTask);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put("/tasks/:id", taskMiddlewares.validateFielStatus, taskMiddlewares.validateFielTitle,tasksController.updateTask);
 
 module.exports = router;
